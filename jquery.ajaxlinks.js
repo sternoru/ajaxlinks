520d76f65577e527f168c12d00e2e2391a9c1cd5
Evgeny Demchenko <slam@sterno.ru>
2012-02-10 20:28:05 +0800

    Initial commit

---
+++ jquery.ajaxlinks.js
@@ -0,0 +1,77 @@
+/* Ajaxlinks jQuery plugin*/
+(function($) {
+    $.fn.ajaxlinks = function(newConfig) {
+    	var defaultConfig = {
+    		load_to: '#content',
+			load_from: null,
+			loader: '<span>Loading...</span>',
+			callback: null,
+			use_sammy: false,
+			prefix: ''
+    	};
+    	var config = $.extend(defaultConfig, newConfig);    	
+		var prevURL = '';
+		var loadCall = function(href) {
+		    var params = {};
+		    if(typeof($.sammy) == 'function' && config.use_sammy == true) {
+		    	var splitPath = href.split('|');			
+				for(var i = 0, ln = splitPath.length; i < ln; i++) {
+					var temp = splitPath[i].split('=');
+					params[temp[0]] = temp[1];
+				}	
+		    }
+		    else {
+		    	params.path = href;
+		    }
+			var ajaxCallback = function() {
+				if(!config.callback) {
+					return;
+				}
+	            config.callback(params);
+	        };			            
+			if(prevURL != params.path) {
+				prevURL = params.path;
+				var path = config.load_from ? [params.path, config.load_from, ' *'].join(' ') : params.path;
+				console.log(path);		
+				$(config.load_to).html($(config.loader)).load(path, ajaxCallback);
+			}
+			else {
+				ajaxCallback();
+			}
+        };
+		
+        if(typeof($.sammy) == 'function' && config.use_sammy == true) {
+            if($.sammy.apps == undefined) {
+				var app = $.sammy(function(){
+					this.get(/\#!(.*)/, function(context){
+						loadCall(window.location.href.split('#!')[1]);
+					});
+				});
+                
+				$(function() {
+	                app.run();
+	            });
+			}
+            			
+            this.each(function(i, item) {
+                var linkHref = $(item).attr('href');
+                
+                if(linkHref != undefined && linkHref.indexOf('#!') == -1) {
+                    $(item).attr('href', [
+                        config.prefix,
+                        '#!path=',
+                        linkHref
+                    ].join(''));
+                }
+            });
+        }
+        else {
+            this.each(function(i, item) {
+                $(item).bind('click', function() {
+            		loadCall($(this).attr('href'));
+            		return false;
+            	});
+        	});
+    	}
+    };
+})(jQuery);
