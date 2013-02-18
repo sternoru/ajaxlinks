0f5310b44bef9f4274fb1d01e190b2da9ccc43b7
Guy BRION <guy.brion@sterno.ru>
2013-02-18 15:58:47 +0800

    fix_forms, add_scripts and ajaxify_content parameters and code added

--- jquery.ajaxlinks.js
+++ jquery.ajaxlinks.js
@@ -11,8 +11,10 @@
 			prefix: '',
 			smooth_height: true,
 			goto_top: false,
-			fix_forms: true
-    	};    	
+			fix_forms: true,
+			add_scripts: false,
+			ajaxify_content: false
+    	};
     	var config = $.extend(defaultConfig, newConfig);
     	config.links = this;    	
 		var prevURL = '';
@@ -40,7 +42,26 @@
 		    else {
 		    	params.path = href;
 		    }
+			var parseScript = function(strcode) {
+				var scripts = new Array();         // Array which will store the script's code
+				
+				// Strip out tags
+				while(strcode.indexOf("<script") > -1 || strcode.indexOf("</script") > -1) {
+					var s = strcode.indexOf("<script");
+					var s_e = strcode.indexOf(">", s);
+					var e = strcode.indexOf("</script", s);
+					var e_e = strcode.indexOf(">", e);
+
+					// Add to scripts array
+					scripts.push(strcode.substring(s_e+1, e));
+					// Strip from strcode
+					strcode = strcode.substring(0, s) + strcode.substring(e_e+1);
+				}
+				
+				return scripts;
+			};
 			var ajaxCallback = function(response, status, xhr) {
+				$(config.load_to).addClass('ajax-loaded');
 				// unfix container's height to adjust it to newly loaded content.
 				// I placed it before the callback in case user is overriding the height.
 				if (config.smooth_height)
@@ -61,26 +82,147 @@
 				if(config.fix_forms) {
 					$('[action="."]').attr('action', href.replace('path=', ''));
 				}
+				if(config.add_scripts) {
+					// Get the scripts src and callbacks
+					var scripts = new Array(),
+						tmp = response,
+						callCB = function(cb) {
+							setTimeout([cb, '();'].join(''), 100);
+						},
+						compare = function(x, y) {
+							if(x === y) { // For reference types : returns true if x and y points to same object
+								return true;
+							}
+							if(x.length != y.length) {
+								return false;
+							}
+							for(key in x) {
+								if(x[key] !== y[key]) { // !== So that the the values are not converted while comparison
+									return false;
+								}
+							}
+							return true;
+						},
+						uniqueArr = function(a) {
+							temp = new Array();
+							for(i = 0; i < a.length; i++) {
+								if(!contains(temp, a[i])) {
+									temp.length += 1;
+									temp[temp.length - 1] = a[i];
+								}
+							}
+							return temp;
+						},
+						contains = function(a, e) {
+							for(j = 0; j < a.length; j++) {
+								if(compare(a[j], e)) {
+									return true;
+								}
+							}
+							return false;
+						};
+					
+					while(tmp.indexOf('<script ') > -1 && tmp.indexOf('></script>') > -1) {
+						var s = tmp.indexOf('<script ');
+						var e = tmp.indexOf('</script>', s);
+						
+						// Add to scripts array
+						if(tmp.substring(s, e).indexOf('src=') > -1) {
+							var src_s = tmp.indexOf('src=', s) + 5;
+							var sep = tmp.substring((src_s - 1), src_s);
+							var src_e = tmp.indexOf(sep, src_s);
+							var script = tmp.substring(src_s, src_e);
+							
+							if(tmp.substring(s, e).indexOf('data-callback=') > -1) {
+								var cb_s = tmp.indexOf('data-callback=', s) + 15;
+								var sep = tmp.substring((cb_s - 1), cb_s);
+								var cb_e = tmp.indexOf(sep, cb_s);
+								var script = [script, tmp.substring(cb_s, cb_e)];
+							}
+							
+							scripts.push(script);
+						}
+						// Strip from tmp
+						tmp = tmp.substring(0, s) + tmp.substring(e + 9);
+					}
+					
+					// Remove the duplicated scripts
+					uniqueArr(scripts);
+					
+					// Load the files that aren't already loaded and call the callbacks of those that are if any
+					for(var i = 0, ln = scripts.length; i < ln; i++) {
+						if(typeof(scripts[i]) != "string") {
+							var src = scripts[i][0];
+							var cb = scripts[i][1];
+						}
+						else {
+							var src = scripts[i];
+							var cb = false;
+						}
+						
+						if($(['head script[src*="', src, '"]'].join('')).length == 0) {
+							$.ajax({
+								url: src,
+								dataType: 'script',
+								cache: true,
+								async: false
+							});
+						}
+						else if(cb) {
+							callCB(cb);
+						}
+					}
+				}
+				if(config.ajaxify_content) {
+					manageLinks($(config.load_to).find('a').filter(":not('.no-al a, a.no-al')"));
+				}
 				if(config.callback) {
 					var link = config.links.filter('[href*="'+href+'"]');
-					config.callback(content, link, params);
+					var scripts = parseScript(response);
+					config.callback(content, link, params, scripts);
 				}
 	        };			            
-			//if(prevURL != params.path) {
-				prevURL = params.path;
-				var path = config.load_from ? [params.path, config.load_from].join(' ') : params.path,
-					$load_to = $(config.load_to);
-				// fix container's height to avoid the glitch
-				if (config.smooth_height)
-				{
-					$load_to.height($load_to.height());
-				}
-				$load_to.html($(config.loader)).load(path, ajaxCallback);				
-			/*}
-			else {
-				ajaxCallback();
-			}*/
+			prevURL = params.path;
+			var path = config.load_from ? [params.path, config.load_from].join(' ') : params.path,
+				$load_to = $(config.load_to);
+			// fix container's height to avoid the glitch
+			if(config.smooth_height)
+			{
+				$load_to.height($load_to.height());
+			}
+			$load_to.html($(config.loader)).load(path, ajaxCallback);				
         };
+		
+		var manageLinks = function(links) {
+			var isExternal = function(url) {
+				return !(url.indexOf(location.href.replace('http://', '').replace('https://', '').split('/')[0]) > -1);
+			};
+			
+			if(config.use_sammy) {
+				links.each(function(i, item) {
+					var linkHref = $(item).attr('href');
+					
+					if(linkHref != undefined && linkHref.indexOf('#!') == -1 && !isExternal($(item)[0].href)) {
+						$(item).attr('href', [
+							config.prefix,
+							'#!path=',
+							linkHref
+						].join(''));
+					}
+				});
+			}
+			else {
+				links.each(function(i, item) {
+					if(!isExternal($(item)[0].href)) {
+						$(item).data('al', 'true');
+						$(item).bind('click', function() {
+							loadCall($(this).attr('href'));
+							return false;
+						});
+					}
+				});
+			}
+		};
 		
         if(typeof($.sammy) == 'function' && config.use_sammy == true) {
             if($.sammy.apps == undefined) {
@@ -94,26 +236,11 @@
 	                app.run();
 	            });
 			}
-            			
-            this.each(function(i, item) {
-                var linkHref = $(item).attr('href');
-                
-                if(linkHref != undefined && linkHref.indexOf('#!') == -1) {
-                    $(item).attr('href', [
-                        config.prefix,
-                        '#!path=',
-                        linkHref
-                    ].join(''));
-                }
-            });
+            
+			manageLinks(this);
         }
         else {
-            this.each(function(i, item) {
-                $(item).bind('click', function() {
-            		loadCall($(this).attr('href'));
-            		return false;
-            	});
-        	});
+            manageLinks(this);
     	}
     };
 })(jQuery);
